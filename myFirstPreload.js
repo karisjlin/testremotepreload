3076a3603d9de850e8567a3779b3ba4b676ab746
Karis L <karisjecille@gmail.com>
2022-03-04 11:54:44 -0600

    update

--- myFirstPreload.js
+++ myFirstPreload.js
@@ -3,6 +3,20 @@ function runPreload() {
 // add your code or functions here and it will be executed
 // as soon as the preload is added
     alert("Hello from a preload");
+    FSBL.Clients.Logger.log("Init context and intent listeners");
+
+    const contextHandler = (context) => {
+        const {ticker = null} = context.id;
+        if(ticker) {
+            FSBL.Clients.Logger.log(`Received contest: `, context);
+            window.location.href = `https://finance.yahoo.com/chart/${ticker}`;
+        } else {
+            FSBL.Clients.Logger.error("context does not have a ticker key");
+        }
+    };
+
+    fdc3.addIntentListener("ViewChart", contextHandler);
+    fdc3.addContextListener("fdc3.instrument", contextHandler);
 }
 
 // this code ensures that the FSBL library has been initialized
